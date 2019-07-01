6c72199d014a255b6dcb86e6225290eced286c18
LizGarciaC <lunaobel@hotmail.com>
2019-06-30 22:03:29 -0500

    Ruteo cambiado por hashroute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Navbar, Container, Row } from 'react-bootstrap';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, HashRouter } from 'react-router-dom';
 import NavLink from 'react-bootstrap/NavLink';
 import Home from "./components/Home";
 import Curriculum from "./components/Curriculum";
@@ -13,25 +13,25 @@ function App() {
   return (
     <div>
       <Navbar expand='sm' bg='light' fixed='top'>
-        <Navbar.Brand href='/'>&lt;LSGC&gt;</Navbar.Brand>
+        <Navbar.Brand href='/#'>&lt;LSGC&gt;</Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
-          <NavLink href='/' >Home</NavLink>
-          <NavLink href='Curriculum'>Curriculum</NavLink>
-          <NavLink href='Projects'>Proyectos</NavLink>
-          <NavLink href='Contact'>Contacto</NavLink>
+          <NavLink href='/#' >Home</NavLink>
+          <NavLink href='#Curriculum'>Curriculum</NavLink>
+          <NavLink href='#Projects'>Proyectos</NavLink>
+          <NavLink href='#Contact'>Contacto</NavLink>
         </Navbar.Collapse>
       </Navbar>
       <Container>
         <Row>&nbsp;</Row>
         <Row>&nbsp;</Row>
         <Row>&nbsp;</Row>
-      <Router>
+      <HashRouter>
         <Route exact path="/" component={Home} />
         <Route path="/Curriculum" component={Curriculum} />
         <Route path="/Projects" component={Projects} />
         <Route path="/Contact" component={Contact} />
-      </Router>
+      </HashRouter>
       </Container>
 
 
