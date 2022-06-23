9e55ed8f1d26689ec0773e92d08c3e9a94409b0c
Liz Garcia <47005857+LizGarciaC@users.noreply.github.com>
2022-06-22 21:48:47 -0500

    Update Curriculum.js

--- src/components/Curriculum.js
+++ src/components/Curriculum.js
@@ -4,7 +4,7 @@ class Curriculum extends React.Component{
     render(){
         return(
            
-            <iframe style={{width: '100%', height: '100%'}} src='https://drive.google.com/file/d/1nlAK7vDeZO-9MW6BMnagiY_IUhlUSUWs/preview?usp=drivesdk' />
+            <iframe style={{width: '100%', height: '100%'}} src='https://drive.google.com/file/d/1Fi7hA0nqTBANfBXIGpBmUd0HIqa9iNCg/view?usp=sharing' />
         )
     }
 }
